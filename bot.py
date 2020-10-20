07a06f7accf8fcef9056e96b1b0d98858d70e724
svcodes <61701181+svcodes@users.noreply.github.com>
2020-10-20 17:04:45 -0500

    do the things

--- bot.py
+++ bot.py
@@ -2,3 +2,16 @@ import discord
 from discord.ext import commands
 
 intents = discord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix = "*", intents = intents)
+
+@client.event
+async def on_ready():
+   print("bot here")
+
+@client.command()
+async def hi(ctx):
+    await ctx.send("hi")
+
+await client.run(TOKEN)
