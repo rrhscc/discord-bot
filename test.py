f374175b63fc0fffce8169ebd14cbffc8958f455
svcodes <suhasvoolla@gmail.com>
2020-10-27 17:23:11 -0500

    do

--- test.py
+++ test.py
@@ -7,13 +7,7 @@ class Tests(commands.Cog):
 
     @commands.command()
     async def ping(self, ctx, *, member: discord.Member = None):
-        """Says hello"""
-        member = member or ctx.author
-        if self._last_member is None or self._last_member.id != member.id:
-            await ctx.send('Ping {0.name}~'.format(member))
-        else:
-            await ctx.send('Ping {0.name}... This feels familiar.'.format(member))
-        self._last_member = member
+        await ctx.send("hello!")
 
 def setup(bot):
     bot.add_cog(Tests(bot))
