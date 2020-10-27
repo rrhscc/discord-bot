632b128868fc1319d460a95214b41389bc3dafba
svcodes <61701181+svcodes@users.noreply.github.com>
2020-10-27 17:34:46 -0500

    Update ping.py

--- ping.py
+++ ping.py
@@ -7,7 +7,7 @@ class Ping(commands.Cog):
       
   @commands.command()
   async def botping(self,ctx):
-      await ctx.send(f"Bot ping is {self.bot.latency}")
+      await ctx.send(f"Bot ping is {self.bot.latency * 1000}")
       
 def setup(bot):
     bot.add_cog(Ping(bot))
