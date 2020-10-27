433035fd66ae7f9a63689c0565f596852c501c85
sithsiri <sith@live.com>
2020-10-27 16:43:54 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+class Tests(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def ping(self, ctx, *, member: discord.Member = None):
+        """Says hello"""
+        member = member or ctx.author
+        if self._last_member is None or self._last_member.id != member.id:
+            await ctx.send('Ping {0.name}~'.format(member))
+        else:
+            await ctx.send('Ping {0.name}... This feels familiar.'.format(member))
+        self._last_member = member
