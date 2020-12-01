a19ffcf5ac16418223a4ca98408d46b6cebdc281
sithsiri <sith@live.com>
2020-12-01 17:20:56 -0600

    Update example.py

--- example.py
+++ example.py
@@ -8,12 +8,12 @@ class Example(commands.Cog):
     @commands.command()
     async def hello(self, ctx, *, member: discord.Member = None):
         msg_split = ctx.message.content.split()
-        if len(msg_split) > 0:
+        try:
             await ctx.send(f'Hello {msg_split[1].name}')
             return
-        
-        member = member or ctx.author
-        await ctx.send(f'Hello {member.name}')
+        except:
+            member = member or ctx.author
+            await ctx.send(f'Hello {member.name}')
 
 def setup(bot):
     bot.add_cog(Example(bot))
