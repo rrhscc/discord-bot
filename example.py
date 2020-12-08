dc82f88fdd36a84da6ba821b141841ba1ddfe701
sithsiri <sith@live.com>
2020-12-08 16:53:43 -0600

    attempt 1

--- example.py
+++ example.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 
+from bot import thing
+
 class Example(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -13,6 +15,10 @@ class Example(commands.Cog):
 
         member = member or ctx.author
         await ctx.send(f'Hello {member.name}!')
-
+    
+    @commands.command()
+    async def testthing(self, ctx):
+        await ctx.send(thing[0])
+    
 def setup(bot):
     bot.add_cog(Example(bot))
