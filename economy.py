9a42b8222146d5cde00ee25b8806812e495b787c
BigTurtle8 <69373245+BigTurtle8@users.noreply.github.com>
2021-01-19 17:26:31 -0600

    Changed burn to give

--- economy.py
+++ economy.py
@@ -48,16 +48,26 @@ class Economy(commands.Cog):
         await ctx.send(f"You have {'${:,.2f}'.format(amt)}")
         
     @commands.command()
-    async def burn(self, ctx, amount: float = None):
+    async def give(self, ctx, amount: float = None, member: discord.Member = None):
         if amount == None or not (isinstance(amount, float) or isinstance(amount, int)):
-            await ctx.send(f"Please specify an amount to burn")
+            await ctx.send(f"Please specify an amount to give.")
             return
         if amount < 0:
-            await ctx.send(f"Please specify a positive amount to burn")
+            await ctx.send(f"Please specify a positive amount to give.")
+            return
+        if member == None:
+            await ctx.send(f"Please specify someone to give your money to.")
             return
         success = await self.withdraw_money(ctx.author, amount)
         if success:
-            await ctx.send(f"Burned successfully")
+            await self.deposit_money(member, amount)
+            
+            embed_var = discord.Embed(description=f'{str(money)} seashells, just for you, {member.display_name}!')
+            embed_var.set_author(name=f'From {ctx.author.display_name}...', icon_url=ctx.author.avatar_url)
+            embed_var.set_thumbnail(url=member.avatar_url)
+
+            await ctx.send(embed=embed_var)
+            
             return
         await ctx.send(f"You do not have enough money to burn.")
     
