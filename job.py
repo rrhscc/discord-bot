fe9b663a17463aea808f88edd8db83a3ae5a797b
Chase Fleeting <darthfleeting@gmail.com>
2021-01-19 17:13:10 -0600

    Create job.py
    Get a job

---
+++ job.py
@@ -0,0 +1,33 @@
+# Get a job
+import discord
+import asyncio
+from discord.ext import commands
+
+class job(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def job(self, ctx, money):
+        m = await ctx.send(f'Get a job. React with ✅ to begin.')
+        await m.add_reaction("✅")
+ 
+                      
+        def check(reaction, user):
+            return user == ctx.author and str(reaction.emoji) == "✅"
+            newJob = 'Doctor'
+
+        try:
+            reaction, user = await self.bot.wait_for('reaction_add', timeout=30.0, check=check)
+        except asyncio.TimeoutError:
+            await m.edit('timed out. :(')
+        else:
+            await ctx.send('You have a new job. Your job is now: ', newJob)
+            economy = self.bot.get_cog('Economy')
+            if economy is not None:
+            await economy.deposit_money(ctx.author, money + 1.25)
+
+            
+
+def setup(bot):
+    bot.add_cog(job(bot))
