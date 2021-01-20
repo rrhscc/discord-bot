de50facb7ad3c99b66923a356b568754c3754b41
sithsiri <sith@live.com>
2021-01-19 18:16:58 -0600

    Update coinflip.py

--- coinflip.py
+++ coinflip.py
@@ -15,7 +15,7 @@ class Coinflip(commands.Cog):
             await ctx.send("Please specify a bet amount. Both you and your opponent will have to have at least this much money.")
             return
         
-        m = await ctx.send(f"{ctx.author.name} has challenged {member.name} to a coinless coinflip! The pool is {$'{:,.2f}'.format(bet*2)} requiring **{$'{:,.2f}'.format(bet)}** from each player.\n{member.mention} must accept with ✅ to coinflip.")
+        m = await ctx.send(f"{ctx.author.name} has challenged {member.name} to a coinless coinflip! The pool is {'${:,.2f}'.format(bet*2)} requiring **{'${:,.2f}'.format(bet)}** from each player.\n{member.mention} must accept with ✅ to coinflip.")
         def check(reaction, user):
             return user == member and str(reaction.emoji) == "✅"
         try:
@@ -35,7 +35,7 @@ class Coinflip(commands.Cog):
                 await economy.deposit_money(ctx.author, bet)
                 return
             winner = random.choice((ctx.author, member))
-            await ctx.send(f"{winner.mention} won! They get {$'{:,.2f}'.format(bet*2)}")
+            await ctx.send(f"{winner.mention} won! They get {'${:,.2f}'.format(bet*2)}")
             await economy.deposit_money(winner, bet*2)
     
 def setup(bot):
