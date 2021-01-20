23444002b6d30b7506705492632a984072aa3e87
sithsiri <sith@live.com>
2021-01-19 18:19:37 -0600

    Update coinflip.py

--- coinflip.py
+++ coinflip.py
@@ -7,7 +7,7 @@ class Coinflip(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def coinflip(self, ctx, *, member: discord.Member = None, bet = None):
+    async def coinflip(self, ctx, *, member: discord.Member = None, bet: Float = None):
         if member is None:
             await ctx.send("Please specify a member to play.")
             return
