c6ebb41767756f91fd80135ffc26db6d6eb6f045
Chase Fleeting <darthfleeting@gmail.com>
2021-03-30 16:38:58 -0500

    blackJack octogon sign???

--- blackJack.py
+++ blackJack.py
@@ -19,7 +19,7 @@ class blackJack(commands.Cog):
            
 
         def check(reaction, user):
-            return user == ctx.author and str(reaction.emoji) == "✅"
+            return user == ctx.author and str(reaction.emoji) == "✅" or str(reaction.emoji) == "🛑"
 
         try:
             reaction, user = await self.bot.wait_for('reaction_add', timeout=30.0, check=check)
@@ -27,28 +27,50 @@ class blackJack(commands.Cog):
             await m.edit(content = 'timed out. :(')
             return
         
-        new_amount = (player_amount + random.randint(0,10))
-        new_house_amount = (house_amount + random.randint(0,10))
+        if (str(reaction.emoji == "✅")):
         
-        if (new_amount > 21):
-            ctx.send(f'Your new amount is: {new_amount}. You lost. Nice job.')
-            economy = self.bot.get_cog('Economy')
-            if economy is not None:
-                await economy.withdraw_money(ctx.author, money)
+            new_amount = (player_amount + random.randint(0,10))
+            new_house_amount = (house_amount + random.randint(0,10))
         
-        elif (new_amount > new_house_amount):
-            ctx.send(f'Your new amount is: {new_amount} and bot amount is: {new_house_amount}. You win. Amazing.')
-            economy = self.bot.get_cog('Economy')
-            if economy is not None:
-                await economy.deposit_money(ctx.author, money * 1.25)
+            if (new_amount > 21):
+                ctx.send(f'Your new amount is: {new_amount}. You lost. Nice job.')
+                economy = self.bot.get_cog('Economy')
+                if economy is not None:
+                    await economy.withdraw_money(ctx.author, money)
+        
+            elif (new_amount > new_house_amount):
+                ctx.send(f'Your new amount is: {new_amount} and bot amount is: {new_house_amount}. You win. Amazing.')
+                economy = self.bot.get_cog('Economy')
+                if economy is not None:
+                    await economy.deposit_money(ctx.author, money * 1.25)
                 
-        else: 
-            ctx.send(f'You lose by the house. RIP.')
-            economy = self.bot.get_cog('Economy')
-            if economy is not None:
-                await economy.withdraw_money(ctx.author, money)
+            else: 
+                ctx.send(f'You lose by the house. RIP.')
+                economy = self.bot.get_cog('Economy')
+                if economy is not None:
+                    await economy.withdraw_money(ctx.author, money)
                 
-        player_amount = new_amount
+            player_amount = new_amount
+           
+         else:
+            while (player_amount > house_amount):
+                new_house_amount = (house_amount + random.randint(0,10))
+                new_house_amount = house_amount
+                if (house_amount > player_amount):
+                    break
+            if (house_amount > 21):
+                ctx.send(f'The houses amount is over 21. You win. Great job.')
+                economy = self.bot.get_cog('Economy')
+                if economy is not None:
+                    await economy.deposit_money(ctx.author, money * 1.25)
+            
+            
+            elif (house_amount > player_amount):
+                ctx.send(f'Your amount is: {player_amount} and the houses amount is: {house_amount}. You lose. Try again.')
+                economy = self.bot.get_cog('Economy')
+                if economy is not None:
+                    await economy.withdraw_money(ctx.author, money)
+            
 
 def setup(bot):
     bot.add_cog(blackJack(bot))
